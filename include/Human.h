9c93358edffdafd186a7021c8680dca287308fa7
dobbystupid <atharit.akk@hotmail.com>
2013-03-23 00:13:46 +0700

    3: Add Human Class

---
+++ include/Human.h
@@ -0,0 +1,25 @@
+#pragma once
+
+//=================================
+// include guard
+#ifndef __HUMAN_H_INCLUDED__
+#define __HUMAN_H_INCLUDED__
+//=================================
+// forward declared dependencies
+
+//=================================
+// included dependencies
+#include "cinder/app/AppBasic.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/Text.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+//=================================
+// the actual class
+
+
+#endif // __CAMERA_H_INCLUDED__ 
+
